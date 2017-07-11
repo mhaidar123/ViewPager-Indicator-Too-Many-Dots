2ffe29b43dcc2754589c59e3a7aba819ddee4f36
Ali Alawieh <ali.alawieh@foo-solutions.com>
2017-07-11 09:10:03 +0300

    Customization

--- example/src/main/java/com/example/vivchar/MainActivity.java
+++ example/src/main/java/com/example/vivchar/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +16,12 @@ import com.example.vivchar.example.R;
 import com.github.vivchar.viewpagerindicator.ViewPagerIndicator;
 
 public
-class MainActivity
-		extends AppCompatActivity
+class MainActivity extends AppCompatActivity
 {
 
 	private ViewPagerIndicator mViewPagerIndicator;
 	private ViewPager mViewPager;
+	private int []imagesRes =new int[] {android.R.color.holo_red_dark,android.R.color.holo_blue_bright,android.R.color.black,android.R.color.holo_green_dark,android.R.color.holo_red_dark,android.R.color.holo_blue_bright,android.R.color.black,android.R.color.holo_green_dark,android.R.color.holo_red_dark,android.R.color.holo_blue_bright};
 
 	@Override
 	protected
@@ -28,17 +29,18 @@ class MainActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+
+
 		mViewPagerIndicator = (ViewPagerIndicator) findViewById(R.id.view_pager_indicator);
 		mViewPager = (ViewPager) findViewById(R.id.view_pager);
 
 		mViewPager.setAdapter(new MyPagerAdapter());
+		mViewPager.setOffscreenPageLimit(5);
 		mViewPagerIndicator.setupWithViewPager(mViewPager);
 		mViewPagerIndicator.addOnPageChangeListener(mOnPageChangeListener);
 	}
 
-	private
-	class MyPagerAdapter
-			extends PagerAdapter
+	private class MyPagerAdapter extends PagerAdapter
 	{
 		@Override
 		public
@@ -48,12 +50,11 @@ class MainActivity
 
 		@SuppressLint("SetTextI18n")
 		@Override
-		public
-		Object instantiateItem(final ViewGroup container, final int position) {
-			final TextView textView = new TextView(MainActivity.this);
-			textView.setText("Page " + position);
-			container.addView(textView);
-			return textView;
+		public Object instantiateItem(final ViewGroup container, final int position) {
+			final ImageView imageView = new ImageView(MainActivity.this);
+			imageView.setImageResource(imagesRes[position]);
+			container.addView(imageView);
+			return imageView;
 		}
 
 		@Override
@@ -87,7 +88,7 @@ class MainActivity
 		@Override
 		public
 		void onPageSelected(final int position) {
-			Toast.makeText(MainActivity.this, "Page selected " + position, Toast.LENGTH_SHORT).show();
+			//Toast.makeText(MainActivity.this, "Page selected " + position, Toast.LENGTH_SHORT).show();
 		}
 
 		@Override
--- viewpagerindicator/src/main/java/com/github/vivchar/viewpagerindicator/ViewPagerIndicator.java
+++ viewpagerindicator/src/main/java/com/github/vivchar/viewpagerindicator/ViewPagerIndicator.java
@@ -1,5 +1,6 @@
 package com.github.vivchar.viewpagerindicator;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
@@ -10,6 +11,9 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -17,36 +21,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** Created by Vitaly Vivchar on 13/01/17. */
-public
-class ViewPagerIndicator
-		extends LinearLayoutCompat
+public class ViewPagerIndicator extends LinearLayoutCompat
 {
 	private static final float SCALE = 1.6f;
 	private static final int NO_SCALE = 1;
 	private static final int DEF_VALUE = 10;
+	private static final int MAX_SHOW = 5;
 
 	private int mPageCount;
 	private int mSelectedIndex;
 	private int mItemSize = DEF_VALUE;
 	private int mDelimiterSize = DEF_VALUE;
+	private ArrayList<View> viewsArrayList = new ArrayList<>();
 
 	@NonNull
 	private final List<ImageView> mIndexImages = new ArrayList<>();
 	@Nullable
 	private ViewPager.OnPageChangeListener mListener;
 
-	public
-	ViewPagerIndicator(@NonNull final Context context) {
+	public ViewPagerIndicator(@NonNull final Context context) {
 		this(context, null);
 	}
 
-	public
-	ViewPagerIndicator(@NonNull final Context context, @Nullable final AttributeSet attrs) {
+	public ViewPagerIndicator(@NonNull final Context context, @Nullable final AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 
-	public
-	ViewPagerIndicator(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr) {
+	public ViewPagerIndicator(@NonNull final Context context, @Nullable final AttributeSet attrs, final int defStyleAttr)
+	{
 		super(context, attrs, defStyleAttr);
 		setOrientation(HORIZONTAL);
 		final TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ViewPagerIndicator, 0, 0);
@@ -61,8 +63,8 @@ class ViewPagerIndicator
 		}
 	}
 
-	private
-	void createEditModeLayout() {
+	private void createEditModeLayout()
+	{
 		for (int i = 0; i < 5; ++i) {
 			final FrameLayout boxedItem = createBoxedItem(i);
 			addView(boxedItem);
@@ -82,53 +84,203 @@ class ViewPagerIndicator
 		viewPager.addOnPageChangeListener(new OnPageChangeListener());
 	}
 
-	public
-	void addOnPageChangeListener(final ViewPager.OnPageChangeListener listener) {
+	public void addOnPageChangeListener(final ViewPager.OnPageChangeListener listener) {
 		mListener = listener;
 	}
 
-	private
-	void setSelectedIndex(final int selectedIndex) {
+	private void setSelectedIndex(final int selectedIndex)
+	{
 		if (selectedIndex < 0 || selectedIndex > mPageCount - 1) {
 			return;
 		}
 
+
+		if(selectedIndex>mSelectedIndex)
+		{
+			if(selectedIndex>2 && selectedIndex<mPageCount-2)
+			{
+				getChildAt(2+selectedIndex).animate().alphaBy(1.0f).setDuration(0).setListener(new Animator.AnimatorListener() {
+
+					@Override
+					public void onAnimationStart(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						getChildAt(2+selectedIndex).setVisibility(View.VISIBLE);
+						getChildAt(2+selectedIndex).setAlpha(0.4f);
+						getChildAt(1+selectedIndex).setAlpha(1.0f);
+					}
+
+					@Override
+					public void onAnimationCancel(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animator animation) {
+
+					}
+				}).start();
+
+
+				getChildAt(selectedIndex-3).animate().alpha(0.0f).setDuration(200).setListener(new Animator.AnimatorListener() {
+					@Override
+					public void onAnimationStart(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						getChildAt(selectedIndex-3).setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onAnimationCancel(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animator animation) {
+
+					}
+				}).start();
+				viewsArrayList.add(getChildAt(2+selectedIndex));
+				viewsArrayList.remove(getChildAt(selectedIndex-3));
+
+
+			}
+			else if(selectedIndex>2)
+			{
+				viewsArrayList.get(viewsArrayList.size()-1).setAlpha(0.4f);
+				getChildAt(0).setAlpha(1.0f);
+			}
+			else if(selectedIndex>mPageCount-3)
+			{
+				viewsArrayList.get(0).setAlpha(0.4f);
+				getChildAt(mPageCount-1).setAlpha(1.0f);
+			}
+			else
+			{
+				viewsArrayList.get(0).setAlpha(0.4f);
+				getChildAt(mPageCount-1).setAlpha(1.0f);
+				getChildAt(0).setAlpha(1.0f);
+
+			}
+
+		}
+		else
+		{
+			if(selectedIndex<mPageCount-3 && selectedIndex>1)
+			{
+				getChildAt(3+selectedIndex).animate().alphaBy(0.0f).setDuration(200).setListener(new Animator.AnimatorListener() {
+					@Override
+					public void onAnimationStart(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						getChildAt(3+selectedIndex).setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onAnimationCancel(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animator animation) {
+
+					}
+				}).start();
+				getChildAt(selectedIndex-2).animate().alphaBy(1.0f).setDuration(0).setListener(new Animator.AnimatorListener() {
+					@Override
+					public void onAnimationStart(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						getChildAt(selectedIndex-2).setVisibility(View.VISIBLE);
+						getChildAt(selectedIndex-2).setAlpha(0.4f);
+						getChildAt(selectedIndex-1).setAlpha(1.0f);
+					}
+
+					@Override
+					public void onAnimationCancel(Animator animation) {
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animator animation) {
+
+					}
+				}).start();
+				viewsArrayList.remove(getChildAt(3+selectedIndex));
+				viewsArrayList.add(getChildAt(selectedIndex-2));
+
+			}
+			else if(selectedIndex>mPageCount-3)
+			{
+				viewsArrayList.get(0).setAlpha(0.4f);
+				getChildAt(mPageCount-1).setAlpha(1.0f);
+			}
+			else if(selectedIndex<=2)
+			{
+				viewsArrayList.get(viewsArrayList.size()-1).setAlpha(0.4f);
+				getChildAt(0).setAlpha(1.0f);
+			}
+			else
+			{
+				viewsArrayList.get(viewsArrayList.size()-1).setAlpha(0.4f);
+				getChildAt(0).setAlpha(1.0f);
+				getChildAt(mPageCount-1).setAlpha(1.0f);
+			}
+
+		}
+
 		final ImageView unselectedView = mIndexImages.get(mSelectedIndex);
+		unselectedView.setImageResource(R.drawable.white_circle);
 		unselectedView.animate().scaleX(NO_SCALE).scaleY(NO_SCALE).setDuration(300).start();
 
 		final ImageView selectedView = mIndexImages.get(selectedIndex);
+		selectedView.setImageResource(R.drawable.blue_circle);
 		selectedView.animate().scaleX(SCALE).scaleY(SCALE).setDuration(300).start();
 
 		mSelectedIndex = selectedIndex;
 	}
 
-	private
-	void setPageCount(final int pageCount) {
+	private void setPageCount(final int pageCount)
+	{
 		mPageCount = pageCount;
 		mSelectedIndex = 0;
 		removeAllViews();
 		mIndexImages.clear();
 
-		for (int i = 0; i < pageCount; ++i) {
+		for (int i = 0; i < pageCount; ++i)
+		{
 			addView(createBoxedItem(i));
+			if(i>MAX_SHOW-1)
+				getChildAt(i).setVisibility(View.GONE);
+			else
+				viewsArrayList.add(getChildAt(i));
 		}
-
 		setSelectedIndex(mSelectedIndex);
 	}
 
 	@NonNull
-	private
-	FrameLayout createBoxedItem(final int position) {
+	private FrameLayout createBoxedItem(final int position)
+	{
 		final FrameLayout box = new FrameLayout(getContext());
 		final ImageView item = createItem();
 		box.addView(item);
 		mIndexImages.add(item);
 
-		final LinearLayoutCompat.LayoutParams boxParams = new LinearLayoutCompat.LayoutParams(
-				(int) (mItemSize * SCALE),
-				(int) (mItemSize * SCALE)
-		);
-		if (position > 0) {
+		final LinearLayoutCompat.LayoutParams boxParams = new LinearLayoutCompat.LayoutParams((int) (mItemSize * SCALE), (int) (mItemSize * SCALE));
+		if (position > 0)
+		{
 			boxParams.setMargins(mDelimiterSize, 0, 0, 0);
 		}
 		box.setLayoutParams(boxParams);
@@ -136,13 +288,10 @@ class ViewPagerIndicator
 	}
 
 	@NonNull
-	private
-	ImageView createItem() {
+	private ImageView createItem()
+	{
 		final ImageView index = new ImageView(getContext());
-		final FrameLayout.LayoutParams indexParams = new FrameLayout.LayoutParams(
-				mItemSize,
-				mItemSize
-		);
+		final FrameLayout.LayoutParams indexParams = new FrameLayout.LayoutParams(mItemSize, mItemSize);
 		indexParams.gravity = Gravity.CENTER;
 		index.setLayoutParams(indexParams);
 		index.setImageResource(R.drawable.white_circle);
@@ -150,9 +299,7 @@ class ViewPagerIndicator
 		return index;
 	}
 
-	private
-	class OnPageChangeListener
-			implements ViewPager.OnPageChangeListener
+	private class OnPageChangeListener implements ViewPager.OnPageChangeListener
 	{
 		@Override
 		public
@@ -179,4 +326,5 @@ class ViewPagerIndicator
 			}
 		}
 	}
+
 }
